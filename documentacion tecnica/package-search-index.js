55157a50397cd1bb40a45fd8ccd8a4bba97b99b5
JVSDAM <145678059+JVSDAM@users.noreply.github.com>
2024-01-14 20:09:45 +0100

    Add files via upload

---
+++ documentacion tecnica/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"swing_c_p02_VargasSeguraJuan","l":"swing_c_p02_VargasSeguraJuan"}];updateSearchResults();
---
+++ documentacion tecnica/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"swing_c_p02_VargasSeguraJuan","l":"BotonPadre"}];updateSearchResults();
